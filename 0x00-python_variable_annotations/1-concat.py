cbdf818493b68ce19bc501c5915a203569901ae7
Ian Kisali <iankisali@gmail.com>
2023-04-28 22:18:29 +0300

    type-annotated concatenation of str

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""type-annotated function concat that takes a string str1
+and string str2 as arguments """
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenating two strings"""
+    return str1 + str2
