25c61c3ad12edf9e033bde543b5ea74675da3cec
Ian Kisali <iankisali@gmail.com>
2023-04-28 22:11:38 +0300

    type-annotated func add taking floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""type-annotated function add that takes
+float a and float b as arguments & returns sum as float"""
+
+
+def add(a: float, b: float) -> float:
+    """ Return sum of two floats"""
+    return a + b
