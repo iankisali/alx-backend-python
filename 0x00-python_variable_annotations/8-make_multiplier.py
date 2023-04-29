54c3fadc652af72775bfbec14519fd6aa22a5bb0
Ian Kisali <iankisali@gmail.com>
2023-04-29 22:00:02 +0300

    return func multipying float by multiplier

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""type-annotated function make_multiplier that takes a float
+multiplier as argument and returns a function that
+multiplies a float by multiplier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """returning function"""
+    def mult(num: float) -> float:
+        """multiplier function"""
+        return num * multiplier
+    return mult
